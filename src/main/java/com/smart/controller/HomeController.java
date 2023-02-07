6540b83056ec3b4988d4cf7fe80e651224b57754
roboanonymous <jsamyak591@gmail.com>
2023-02-07 19:09:23 +0530

    first

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -2,7 +2,9 @@ package com.smart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.smart.dao.UserRepository;
@@ -11,5 +13,18 @@ import com.smart.entities.User1;
 @Controller
 public class HomeController {
 
+	@RequestMapping("/home")
+	public String home(Model model)
+	{
+		model.addAttribute("title", "Home - smart contact Manager");
+		return "home";
+	}
+	
+	@RequestMapping("/about")
+	public String about(Model model)
+	{
+		model.addAttribute("title", "About - smart contact Manager");
+		return "about";
+	}
 	
 }
