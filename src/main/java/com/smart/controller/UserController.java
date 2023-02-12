ce3f3970d179bc043a81029c57b4030ea4c47c8d
roboanonymous <jsamyak591@gmail.com>
2023-02-12 19:05:46 +0530

    first

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,15 +1,33 @@
 package com.smart.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User1;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	
+	@Autowired
+	public UserRepository UserRepository;
+	
+	
+	/* User home handler*/
 	@RequestMapping("/index")
-	public String dashboard()
+	public String dashboard(Model model , Principal principal)
 	{
+		String username = principal.getName();
+		
+		User1 user1 =   UserRepository.getUserbyUsername(username);
+		
+		model.addAttribute("user1", user1);	
+		
 		return "normal/user_dashboard";
 	}
 
