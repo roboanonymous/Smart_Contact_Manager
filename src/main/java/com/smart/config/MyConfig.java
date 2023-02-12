31f845b294f57c8459d39a88274e07db5f66d273
roboanonymous <jsamyak591@gmail.com>
2023-02-12 12:11:50 +0530

    first

--- src/main/java/com/smart/config/MyConfig.java
+++ src/main/java/com/smart/config/MyConfig.java
@@ -1,8 +1,86 @@
 package com.smart.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class MyConfig {
 
+	@Bean
+	public UserDetailsService UserDetailsService()
+	{
+		return new UserDetailSrviceImpel();
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider()
+	{
+		DaoAuthenticationProvider DaoAuthenticationProvider = new DaoAuthenticationProvider();
+		DaoAuthenticationProvider.setUserDetailsService(this.UserDetailsService());
+		DaoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+		
+		return DaoAuthenticationProvider;
+	}
+	
+	
+	// Configure Method ... 
+	
+	@Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+	
+	
+	@Bean
+	public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception
+	{
+		http.
+		authorizeHttpRequests()
+		.requestMatchers("/admin/**").hasRole("ADMIN")
+		.requestMatchers("/user/**").hasRole("USER")
+		.requestMatchers("/**").permitAll().and().formLogin()
+		.loginPage("/signin")
+		.loginProcessingUrl("/login-process").
+		defaultSuccessUrl("/user/index")
+		.and().csrf().disable();
+		
+		http.authenticationProvider(authenticationProvider());
+		
+		DefaultSecurityFilterChain build = http.build();
+		
+		return build;
+	}
+	
+	/*
+	 * protected void configure(AuthenticationManagerBuilder auth) throws Exception
+	 * { auth.authenticationProvider(this.authenticationProvider()); }
+	 * 
+	 * 
+	 * @Override protected void configure(HttpSecurity http) throws Exception {
+	 * http.authorizeRequests() .antMatchers("/admin/**").hasRole("ADMIN")
+	 * .antMatchers("/user/**").hasRole("USER")
+	 * .antMatchers("/**").permitAll().and().formLogin() .loginPage("/signin")
+	 * .loginProcessingUrl("/login-process"). defaultSuccessUrl("/user/index")
+	 * .and().csrf().disable(); }
+	 */
+	
 }
