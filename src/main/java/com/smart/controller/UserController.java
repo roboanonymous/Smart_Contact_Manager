28feb17ec782d2646474155cf41fd1dbe794b090
roboanonymous <jsamyak591@gmail.com>
2023-02-13 16:01:46 +0530

    first

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.smart.controller;
 
+import java.io.File;
 import java.security.Principal;
+import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.Contact;
 import com.smart.entities.User1;
+
+import jakarta.persistence.criteria.Path;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/user")
@@ -57,6 +67,18 @@ public class UserController {
 		model.addAttribute("contact",new Contact());	
 		return "normal/add_contact_form";
 	
+	}
+	
+	/**
+	 *Add contact-form-data processing to store in
+	 *respective users account
+	 **/
+	@PostMapping("/process-contact")
+	public String processAddContact( @ModelAttribute Contact contact) {
+
+		System.out.println("Data  " + contact);
+
+		return "normal/add_contact_form";
 	}
 
 }
--- src/main/java/com/smart/entities/Contact.java
+++ src/main/java/com/smart/entities/Contact.java
@@ -83,6 +83,12 @@ public class Contact {
 	public void setDescription(String description) {
 		this.description = description;
 	}
+	@Override
+	public String toString() {
+		return "Contact [cID=" + cID + ", name=" + name + ", secondName=" + secondName + ", work=" + work + ", email="
+				+ email + ", phone=" + phone + ", image=" + image + ", description=" + description + ", user1=" + user1
+				+ "]";
+	}
 	
 	
 	
